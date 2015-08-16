fc01e8b68fe487c4e7cd3f97b91125ccbe794a3f
hubatish <zachariah.howell@gmail.com>
2015-08-16 18:32:37 -0400

    Added a countdown timer and a bad placeholder ending screen for end of round

---
+++ Assets/Maze/Scripts/RoundManager.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using UnityEngine;
+
+public class RoundManager : MonoBehaviour
+{
+    public GameObject startScreen;
+    public GameObject endScreen;
+
+    public CountdownDisplay countdownTimer;
+
+    protected void Start()
+    {
+        countdownTimer.doneEvent += delegate ()
+        {
+            endScreen.SetActive(true);
+            GridManager.Instance.gameObject.SetActive(false);
+        };
+    }
+}
+
